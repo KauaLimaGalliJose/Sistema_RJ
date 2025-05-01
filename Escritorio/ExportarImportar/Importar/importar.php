fe5744e2d042a819a422f53edfcf534162f02093
Kauasite <kaualimagallik@gmail.com>
2025-05-01 17:37:30 -0300

    01/05

--- Escritorio/ExportarImportar/Importar/importar.php
+++ Escritorio/ExportarImportar/Importar/importar.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../../coroa.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+  
+</body>
+</html>
 <?php 
 //imports
 include_once('./lerAquivos.php');
@@ -16,14 +28,16 @@ if (isset($_FILES['dadosImport']) && $_FILES['dadosImport']['error'] === UPLOAD_
     $zip->extractTo($extractTo);
     $zip->close();
 
-    echo "Arquivos extraídos com sucesso!";
+    echo "Arquivos extraídos com sucesso!";?><br><?php
     unlink('./zipTemporarios/zipTemporario.zip');
   }
 
+  ler_Salvar_csv('pedidosp',$conectar);
+  ler_Salvar_csv('pedidospg',$conectar);
+  ler_Salvar_csv('pedidospe',$conectar);
 
 } else {
-  echo 'Arquivo não enviado ou ocorreu um erro no upload.';
+  echo 'Arquivo não enviado ou ocorreu um erro no upload.'; ?><br><?php
 }
 
-ler_Salvar_csv('pedidosp',$conectar);
 ?>
--- Escritorio/ExportarImportar/Importar/lerAquivos.php
+++ Escritorio/ExportarImportar/Importar/lerAquivos.php
@@ -2,19 +2,69 @@
 
 function ler_Salvar_csv($tabela,$conectar){
 
-    $enviarDados = fopen('./zipTemporarios/pastas/csv/' . $tabela . '.csv', 'r');
+    $arquivo = './zipTemporarios/pastas/csv/' . $tabela . '.csv';
+
+    //Carcacter do Pedido
+    $caracter = $tabela;
+    $caractereMaiusculo = strtoupper($caracter[0]); // 'strtoupper' Pega o primeiro caractere e transforma em maiúsculo
+
+    if (file_exists($arquivo)) {
+
+        $enviarDados = fopen('./zipTemporarios/pastas/csv/' . $tabela . '.csv', 'r');
+
+    } 
+    else{
+        $enviarDados = false ;
+    }
 
     if ($enviarDados !== false) {
         
-        $cabecalho = fgetcsv($enviarDados, 10000, ",");     
-        print_r($cabecalho[0]);
+        $cabecalho = fgetcsv($enviarDados, 10000, ",");    
 
         while (($linha = fgetcsv($enviarDados, 10000, ",")) !== false) {
             // $linha é um array com os dados da linha do CSV
-         $dados = mysqli_query($conectar, "INSERT INTO `$tabela`
-            (`$cabecalho[0]`,`$cabecalho[1]`,`$cabecalho[2]`, `$cabecalho[3]`, `$cabecalho[4]`, `$cabecalho[5]`, `$cabecalho[6]`, `$cabecalho[7]`,`$cabecalho[8]`, `$cabecalho[9]`, `$cabecalho[10]`,`$cabecalho[11]`,`$cabecalho[12]`,`$cabecalho[13]`,`$cabecalho[14]`,`$cabecalho[15]`, `$cabecalho[16]`,`$cabecalho[17]`) 
-            VALUES ('$linha[0]','$linha[1]','$linha[2]', '$linha[3]', '$linha[4]', '$linha[5]', '$linha[6]', '$linha[7]','$linha[8]', '$linha[9]', '$linha[10]','$linha[11]' , '$linha[12]' ,'$linha[13]', '$linha[14]' , '$linha[15]' , '$linha[16]' , '$linha[17]' )");
+            
+            //VARIAVEL PARA DATA PARA CONSULTA NO BANCO DE DADOS
+            $datacsv = $linha[17];
+
+            $result = $conectar->query(
+                "SELECT *
+                 FROM `$tabela` WHERE `$cabecalho[0]` <> 0 
+                 AND data_digitada LIKE '$datacsv'
+                 ORDER BY `$cabecalho[0]` ASC"
+                );
+
+            if($result->num_rows !== 0){
+
+                while($verificador = mysqli_fetch_assoc($result)){
+
+                    if($verificador['idpedidos'] !== $linha[1]){
+
+                        print_r($linha[1]);
+
+                        mysqli_query($conectar, "INSERT INTO `$tabela`
+                        (`$cabecalho[0]`,`$cabecalho[1]`,`$cabecalho[2]`, `$cabecalho[3]`, `$cabecalho[4]`, `$cabecalho[5]`, `$cabecalho[6]`, `$cabecalho[7]`,`$cabecalho[8]`, `$cabecalho[9]`, `$cabecalho[10]`,`$cabecalho[11]`,`$cabecalho[12]`,`$cabecalho[13]`,`$cabecalho[14]`,`$cabecalho[15]`, `$cabecalho[16]`,`$cabecalho[17]`) 
+                        VALUES ('$linha[0]','$linha[1]','$linha[2]', '$linha[3]', '$linha[4]', '$linha[5]', '$linha[6]', '$linha[7]','$linha[8]', '$linha[9]', '$linha[10]','$linha[11]' , '$linha[12]' ,'$linha[13]', '$linha[14]' , '$linha[15]' , '$linha[16]' , '$datacsv' )");
+                    
+                    }
+                    else{
+                        echo 'Já tem pedidos existentes';?><br><?php
+                        break;
+                    }
+                }
+            }
+            else{
+                echo ' Erro não há pedidos';?><br><?php
+                break;
+            }
+            print('Arquivo Salvo com Sucesso!!');?><br><?php
+            break;
         }
+
+        unlink('./zipTemporarios/pastas/csv/' . $tabela . '.csv');
+    }
+    else{
+        echo 'Falha sem pedidos ou arquivo danificado'; ?><br><?php
     }
 }
 ?>
